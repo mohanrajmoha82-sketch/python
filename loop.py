b0367f5962f50d996a49bca08ac37a8337ca04f4
Mohanraj.M <mohanrajmoha82@gmail.com>
2026-03-26 08:51:55 -0700

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,70 @@
+#loop
+'''
+a=[2,3,4,6]#list
+print(7 in a)
+print(4 in a)
+print(4 not in a)
+print(7 not in a)
+'''
+#for loop
+'''
+1.for-integer => inc/dec
+2.for-string
+'''
+#inc
+#1
+'''
+for i in range(1,6,1):#12345
+    print(i)
+    
+#2
+
+for i in range(0,51,5):#0 to 50
+    print(i)
+
+#3
+
+for i in range(40,51):
+    print(i)
+
+#4
+
+for i in range(51):
+    print(i)
+'''
+
+#for loop => interger method -dec
+'''
+for i in range(10,5,-2):#109876#6-2=4
+    print(i)
+ '''
+#python demo taks
+#1. factorial=>5,6
+'''
+5*4*3*2*1=120
+6*5*4*3*2*1=720
+'''
+'''
+n=int(input("Enter value :"))#5
+fact=1
+for i in range(1,n+1):
+    fact*=i
+print("factoral value :",fact)
+'''
+#sum of natural value upto 10
+
+n=int(input("Enter value :"))#5
+s=0
+for i in range(1,n+1):#12345
+    s+=i
+
+    print("Enter value :",s)
+
+
+
+
+    
+
+
+    
+
