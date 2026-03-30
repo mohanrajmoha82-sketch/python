d2d94d676702e1a50145fbb32e59652b2f078545
Mohanraj.M <mohanrajmoha82@gmail.com>
2026-03-29 23:48:41 -0700

    Add files via upload

---
+++ basic taks (int).py
@@ -0,0 +1,76 @@
+#Q1
+
+print("Hello world")
+
+#Q2. Write a python to do arithmatical operations addition and division
+#addition
+
+a=int(input("Enter the number :"))
+b=int(input("Enter the number :"))
+c=a+b
+print("a,b addition value :",c)
+
+#division
+
+a=int(input("Enther the number :"))
+b=int(input("Enter the number :"))
+c=a/b
+print("a,b division volue :",c)
+
+#Q3
+"""
+triangle areas
+1/2xbxh
+"""
+base=float(input("Enter the triangle bass :"))
+height=float(input("Enter the striangle height :"))
+area=1/2*base*height
+print("the area of the triangle :",area)
+
+#Q4
+#swap to variables.
+
+a=int(input("Enter the value :"))
+b=-int(input("Enter the value :"))
+s=a#5+9=14
+a=b #14+5=
+b=s
+print("Swap value")
+print(a,b)
+
+#Q5
+#without swap
+
+a=int(input())
+b=int(input())
+s=a+b#5+9=14
+a=s-a#14-5=9
+b=s-b#9-14=5
+print(a)
+print(b)
+
+
+#Q6
+"""
+kilometer to miles
+miles=kilometer*o.621371
+"""
+
+ks=int(input("Enter the kelometer :" "ks"))
+miles=ks*0.621371
+print(miles)
+
+#Q7
+"""
+celsius to fahrenheit
+F=(Cx(9/5))+32
+"""
+C=int(input("Enter the celsius :"))
+F=(C*9/5)+32
+print(C," degrees celsiuse is equal to",F ," drgrees fahreheit")
+
+
+
+
+
+
