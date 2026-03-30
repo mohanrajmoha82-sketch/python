e57d4ed842ea4b3f9eb8202ba3355c190901aa71
Mohanraj.M <mohanrajmoha82@gmail.com>
2026-03-30 00:06:58 -0700

    Add files via upload

---
+++ loop string.py
@@ -0,0 +1,32 @@
+#for variable in string_data:
+     #looping statement
+'''
+a="python"
+for i in a:
+    print(i)
+'''
+'''
+for j in 'python':
+    print(j)
+'''
+'''
+for i in "mohanraj":          #[key word=end=""]
+    print(i,end="")
+'''
+'''
+#pattorn
+for i in range(5):
+    for i in range(5):
+        print('*',end=" ")
+    print()
+'''
+n=int(input("enter value :"))
+for i in range(n):
+      for i in range(n):
+          print(i,end=" ")
+      print()
+        
+        
+        
+            
+     
